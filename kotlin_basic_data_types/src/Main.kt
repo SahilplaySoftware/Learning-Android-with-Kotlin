145273544251aa6cfda1b0dd581137793ac6ae66
Abrar <abrarhamim2@gmail.com>
2024-02-14 15:05:42 +0600

    learned basic data types of kotlin

---
+++ kotlin_basic_data_types/src/Main.kt
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    //Basic kotlin data types
+    val name: String = "Sahil"
+    val age: Int = 25
+    val height: Double = 5.8
+    val isMarried: Boolean = false
+
+    println("Name: $name")
+    println("Age: $age")
+    println("Height: $height")
+    println("Married: $isMarried")
+}
